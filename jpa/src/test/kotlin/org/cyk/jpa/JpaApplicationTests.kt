0571c19213179cf36673f177f1904e21c4d8524f
cyk318 <chenyikang2022@163.com>
2024-05-02 20:40:53 +0800

    init

---
+++ jpa/src/test/kotlin/org/cyk/jpa/JpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.cyk.jpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
