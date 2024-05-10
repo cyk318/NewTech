735eca79efc5759b2228356083e1ea2e530a9044
cyk318 <chenyikang2022@163.com>
2024-05-10 17:35:06 +0800

    init

---
+++ es/src/test/kotlin/org/cyk/es/EsApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.cyk.es
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
