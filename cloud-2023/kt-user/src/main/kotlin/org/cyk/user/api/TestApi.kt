9276076b5b479cda5c458852f3072c496c10f008
cyk318 <chenyikang2022@163.com>
2024-05-12 22:38:05 +0800

    feat: sentinel

--- cloud-2023/kt-user/src/main/kotlin/org/cyk/user/api/TestApi.kt
+++ cloud-2023/kt-user/src/main/kotlin/org/cyk/user/api/TestApi.kt
@@ -1,5 +1,6 @@
 package org.cyk.user.api
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
