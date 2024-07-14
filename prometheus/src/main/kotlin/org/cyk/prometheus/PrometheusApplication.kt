0227bb199b6ed9d67513e302559e0fc6516312c1
cyk318 <chenyikang2022@163.com>
2024-07-14 17:38:07 +0800

    feat: prometheus 集成

---
+++ prometheus/src/main/kotlin/org/cyk/prometheus/PrometheusApplication.kt
@@ -0,0 +1,11 @@
+package org.cyk.prometheus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrometheusApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrometheusApplication>(*args)
+}
---
+++ prometheus/src/test/kotlin/org/cyk/prometheus/PrometheusApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.cyk.prometheus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PrometheusApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
