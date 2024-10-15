119abfb6bff20b2f7da0547c6f34debd29f3f71d
cyk318 <chenyikang2022@163.com>
2024-10-15 22:38:37 +0800

    接口限流设计完成

--- kt-earth/src/main/kotlin/org/cyk/ktearth/service/FlowLimitForRedisService.kt
+++ kt-earth/src/main/kotlin/org/cyk/ktearth/service/FlowLimitForRedisService.kt
@@ -1,5 +1,6 @@
 package org.cyk.ktearth.service
 
+import org.slf4j.LoggerFactory
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 import java.util.concurrent.TimeUnit
@@ -7,21 +8,22 @@ import java.util.concurrent.TimeUnit
 /**
  * 限流设计:
  * 描述: 对所有非管理员的写接口进行限流
- * 技术: 使用 redis 实现限流  key:{ LimitType.id + 用户id }  value: 操作次数
+ * 技术: 使用 redis 实现限流  key:{ LimitType.id + ":" + 用户id }  value: 操作次数
  * Ps: 由于不开放注册接口，因此使用户id一定不为空
  */
 
 enum class LimitType(
-    id: String, // 限流类型唯一标识
-    maxReqCnt: Int, // 单位时间内，最大请求数
-    timeout: Long, // 超时时间
-    unit: TimeUnit // 超时时间单位
+    val id: String, // 限流类型唯一标识
+    val maxReqCnt: Int, // 单位时间内，最大请求数
+    val timeout: Long, // 超时时间
+    val unit: TimeUnit // 超时时间单位
 ) {
     //TODO 文章发布
     /**
      * 访问量增加: 一分钟内最多 +1
      */
-    VIEW_POST("view_post", 1, 1, TimeUnit.MINUTES)
+    VIEW_POST("view_post", 1, 1, TimeUnit.MINUTES),
+    ;
 }
 
 interface FlowLimitForRedisService {
@@ -34,13 +36,31 @@ class FlowLimitForRedisServiceImpl(
 ): FlowLimitForRedisService {
 
     override fun entry(userId: String, type: LimitType): Boolean {
-        TODO()
-        //1.key 不存在
-        // 1) 设置 value，然后返回 true
+        val key = type.id + ":" + userId
+        val beforeCnt = redisTemplate.opsForValue().get(key)
+
+        if (beforeCnt == null) { //1.key 不存在
+            // 1) 设置 value，然后返回 true
+            redisTemplate.opsForValue().set(key, "1", type.timeout, type.unit)
+            return true
+        }
+
         //2.key 存在
-        // 1) value + 1
-        // 2) 检查没有超过 maxReqCnt，返回 true
-        // 3) 超过 maxReqCnt, 检查如果是最大次数的 10 倍，那么就 warn 日志，最后返回 false
+        //1) value + 1
+        val afterCnt = redisTemplate.opsForValue().increment(key)!!
+        //2) 检查没有超过 maxReqCnt，返回 true
+        if (afterCnt <= type.maxReqCnt) {
+            return true
+        }
+        //3) 超过 maxReqCnt, 检查如果是最大次数的 10 倍，那么就 warn 日志，最后返回 false
+        if (afterCnt > type.maxReqCnt * 10) {
+            log.warn("目标被限流，且疑似有刷接口嫌疑  redisKey: $key ")
+        }
+        return false
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(FlowLimitForRedisServiceImpl::class.java)
     }
 
 }
