fa3bd094f9e777c896632986bd2904bb750873de
cyk318 <chenyikang2022@163.com>
2024-10-20 23:23:30 +0800

    点赞领域模型完成

--- kt-earth/src/main/kotlin/org/cyk/ktearth/infra/repo/uact/UserLikeRepoImpl.kt
+++ kt-earth/src/main/kotlin/org/cyk/ktearth/infra/repo/uact/UserLikeRepoImpl.kt
@@ -1,17 +1,19 @@
 package org.cyk.ktearth.infra.repo.uact
 
 import org.cyk.ktearth.domain.uact.domain.Like
+import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.stereotype.Repository
 import java.util.*
 
-@Document
+@Document("user_likes")
 data class UserLikesDo(
+    @Id
     val userId: String,
-    val likes: MutableList<Like> = mutableListOf(),
-    val cTime: Date = Date(),
-    var uTime: Date = Date(),
+    val likes: MutableList<Like>,
+    val cTime: Date,
+    var uTime: Date,
 )
 
 @Repository
